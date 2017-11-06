2b31f1caee9460f3f95193770aa38f03bd4207e3
John <jc.honkanen@gmail.com>
2017-11-06 00:10:22 +0000

    Added flat lighting back

--- MobaJuiceEngine/Engine/Component/MeshRenderer.cpp
+++ MobaJuiceEngine/Engine/Component/MeshRenderer.cpp
@@ -43,9 +43,15 @@ namespace Engine {
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDisable(GL_CULL_FACE);*/
 
+		glm::vec3 objectColor(1.0f, 0.5f, 0.31f);
+		glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+		glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 		glUniformMatrix4fv(glGetUniformLocation(shader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 		glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(glGetUniformLocation(shader, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniform3fv(glGetUniformLocation(shader, "objectColor"), 1, glm::value_ptr(objectColor));
+		glUniform3fv(glGetUniformLocation(shader, "lightColor"), 1, glm::value_ptr(lightColor));
+		glUniform3fv(glGetUniformLocation(shader, "lightPos"), 1, glm::value_ptr(lightPos));
 		//ENDOFMVP
 		mesh->Render();
 	}
