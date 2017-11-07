f9ee0efa95ba1e15b0e3cdc64dc6fb5e489c147c
Lapalm <anthony_palm@hotmail.co.jp>
2017-11-07 21:14:21 +0000

    Cleaned raycast

--- MobaJuiceEngine/Engine/Physics/RayCast.cpp
+++ MobaJuiceEngine/Engine/Physics/RayCast.cpp
@@ -2,7 +2,7 @@
 #include "..\Component\Camera.h"
 
 namespace Engine {
-	RayCast::RayCast(float output, float input)
+	RayCast::RayCast()
 	{
 	}
 
@@ -11,28 +11,9 @@ namespace Engine {
 	{
 	}
 
-	float RayCast::getRayAngle(float currentRayAngle)
+	RayCast::RayCast(glm::vec3 startPoint)
 	{
-		return currentRayAngle;
+		point1 = startPoint;
 	}
 
-	float RayCast::getRayLength(float rayLength)
-	{
-		return rayLength;
-	}
-
-	glm::vec2 RayCast::getP1Posi(glm::vec2 p1Pos)
-	{
-		// set as camera position
-
-		p1Pos = Camera::mainCamera->transform->GetPosition();
-		return p1PoS;
-	}
-
-	glm::vec2 RayCast::getP2Posi(glm::vec2 p2Pos)
-	{
-		// Position of collision
-		p2Pos = getP1Posi(p1PoS) + getRayLength(rayLength) * glm::vec2((sinf(currentRayAngle)), cosf(currentRayAngle));
-		return p2Pos;
-	}
 }
--- MobaJuiceEngine/Engine/Physics/RayCast.h
+++ MobaJuiceEngine/Engine/Physics/RayCast.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <glm\glm.hpp>
-
+#include <SDL.h>
 
 using namespace std;
 
@@ -8,28 +8,14 @@ namespace Engine {
 	class RayCast
 	{
 	private:
+		glm::vec3 point1;
 
-		bool isColision = false;
-		float rayLength = 10.0f;
-		float maxFraction; // Specifies how far along the ray should be checked for an intersection.
-		float currentRayAngle = 0;
-
-		glm::vec2 p1PoS; // Point 1 Position <--- The camera
-		glm::vec2 p2Pos; // Point 2 Position <--- Intersection 
-		glm::vec2 normal; 
-
-		double curTime = 0; // Time of current frame
-		double prevTime = 0; // Time of previous frame
 
 	public:
-		RayCast(float output, float input);
+		RayCast();
 		~RayCast();
 
-		float getRayAngle(float currentRayAngle);
-		float getRayLength(float rayLength);
-
-		glm::vec2 getP1Posi(glm::vec2 p1Pos); // Position of point1 is at Camera position.
-		glm::vec2 getP2Posi(glm::vec2 p2Pos); // Position of intersection
+		RayCast(glm::vec3 startPoint);
 	};
 }
 
