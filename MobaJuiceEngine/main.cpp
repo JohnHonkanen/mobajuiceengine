4a7de82488d228035ff5b5d7269b9b7433740f43
John <jc.honkanen@gmail.com>
2017-12-03 17:06:23 +0000

    Main

--- MobaJuiceEngine/main.cpp
+++ MobaJuiceEngine/main.cpp
@@ -17,9 +17,9 @@ int main(int argc, char *argv[]){
 	GameEngine::manager.inputManager.AddKey("FreezeMouse", "f", "g");*/
 
 	engine.LoadSettings("default-settings.xml");
-	engine.Load("BaseScene.xml");
+	//engine.Load("BaseScene.xml");
 
-	Scene *scene = engine.GetActiveScene();
+	Scene *scene = engine.CreateScene("Demo Scene");
 	scene->AddSkyBox("Assets/Skybox/Nebula/Nebula.tga");
 	GameObjectManager *list = scene->GetGameObjectManager();
 
@@ -27,7 +27,9 @@ int main(int argc, char *argv[]){
 	Terrain * t = Terrain::Create(terrain, 10, 20, 20, 0.003f, 10.0f, "terrainShader");
 	t->material.diffuseMap = "Assets/Textures/ground.jpg";
 
-	GameObject *camera = list->Find("Camera_1");
+	GameObject *camera = list->CreateGameObject("Camera");
+	Camera *c = Camera::Create(camera);
+	c->SetFarClippingPlane(1000);
 	camera->transform->Translate(vec3(0.0f, 20.0f, -45.0f));
 	camera->transform->SetEulerAngle(vec3(45.0f, 0.0f, 0.0f));
 
@@ -41,8 +43,16 @@ int main(int argc, char *argv[]){
 	GameObject *rock = list->CreateGameObject("rock");
 	MeshRenderer::Create(rock, "Assets/Models/rock/Rock.obj");
 	TextureSetter::Create(rock, "Assets/Textures/Rock/Rock_d.jpg");
-	rock->transform->SetScale(vec3(0.05f));
+	rock->transform->SetScale(vec3(0.3f));
+	rock->transform->Rotate(vec3(-60, 0, 0));
 	rock->transform->SetPosition(-10, t->GetHeight(-10, 20), 20);
+
+	rock = list->CreateGameObject("rock");
+	MeshRenderer::Create(rock, "Assets/Models/rock/Rock.obj");
+	TextureSetter::Create(rock, "Assets/Textures/Rock/Rock_d.jpg");
+	rock->transform->SetScale(vec3(0.2f));
+	rock->transform->Rotate(vec3(0, 0, 0));
+	rock->transform->SetPosition(30, t->GetHeight(30, -5), -30);
 
 
 	engine.Run();
